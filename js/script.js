1d315edae0abcccc11330e07974093352494bcfb
nouraa17 <nouranana31@gmail.com>
2024-08-04 21:27:09 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,30 +1,44 @@
 /// code for the video
 
-document.getElementById('play-button').onclick = function() {
-    var video = document.getElementById('video-player');
-    var imageVideo = document.getElementById('image-video-container');
-    var videoContainer = document.getElementById('video-container');
-    imageVideo.style.display = 'none';
-    videoContainer.style.display = 'block';
-    video.play();
-};
-document.getElementById('pause-button').onclick = function() {
-    var video = document.getElementById('video-player');
-    if (video.paused) {
-        video.play();
-        event.target.textContent = 'Pause';
-    } else {
-        video.pause();
-        event.target.textContent = 'Play';
-    }
-};
+// document.getElementById('play-button').onclick = function() {
+//     var video = document.getElementById('video-player');
+//     var imageVideo = document.getElementById('image-video-container');
+//     var videoContainer = document.getElementById('video-container');
+//     imageVideo.style.display = 'none';
+//     videoContainer.style.display = 'block';
+//     video.play();
+// };
+// document.getElementById('pause-button').onclick = function() {
+//     var video = document.getElementById('video-player');
+//     if (video.paused) {
+//         video.play();
+//         event.target.textContent = 'Pause';
+//     } else {
+//         video.pause();
+//         event.target.textContent = 'Play';
+//     }
+// };
+// document.getElementById('close-button').onclick = function() {
+//     var video = document.getElementById('video-player');
+//     var imageVideo = document.getElementById('image-video-container');
+//     var videoContainer = document.getElementById('video-container');
+//     video.pause();
+//     videoContainer.style.display = 'none';
+//     imageVideo.style.display = 'block';
+// };
+
 document.getElementById('close-button').onclick = function() {
     var video = document.getElementById('video-player');
-    var imageVideo = document.getElementById('image-video-container');
-    var videoContainer = document.getElementById('video-container');
+    var modal = document.getElementById('videoModal');
     video.pause();
-    videoContainer.style.display = 'none';
-    imageVideo.style.display = 'block';
+    modal.hide();
+};
+
+document.getElementById('play-button').onclick = function() {
+    var videoModal = new bootstrap.Modal(document.getElementById('videoModal'));
+    videoModal.show();
+    var video = document.getElementById('video-player');
+    video.play();
 };
 
 /////////////////////////////////////////////////////////////////////////////////////
